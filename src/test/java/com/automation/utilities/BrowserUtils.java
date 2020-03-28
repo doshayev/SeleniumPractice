df9cd54c8d3d35d0722e29f0b5738c5af9ac9c70
Adil Doshayev <doshayev@gmail.com>
2020-03-28 10:33:59 -0500

    update

---
+++ src/test/java/com/automation/utilities/BrowserUtils.java
@@ -0,0 +1,31 @@
+package com.automation.utilities;
+
+import org.openqa.selenium.WebElement;
+import java.util.ArrayList;
+import java.util.List;
+public class BrowserUtils {
+    /**
+     * Pause test for some time
+     *
+     * @param seconds
+     */
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(1000 * seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static List<String> getTextFromWebElements(List<WebElement> elements) {
+        List<String> textValues = new ArrayList<>();
+        for (WebElement element : elements) {
+            textValues.add(element.getText());
+        }
+        return textValues;
+    }
+}
+
+
+
+
+
