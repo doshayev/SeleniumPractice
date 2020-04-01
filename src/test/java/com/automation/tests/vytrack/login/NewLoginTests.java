1285ec18bbededb952fb5ac9e02b8cfed409f7ec
Adil Doshayev <doshayev@gmail.com>
2020-03-31 20:45:05 -0500

    update

--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -18,7 +18,8 @@ public class NewLoginTests extends AbstractTestBase {
         test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        test.info("Login as store manager");
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboar");
         //if assertion passed, it will set test status in report to passed
         test.pass("Page title Dashboard was verified");
     }
