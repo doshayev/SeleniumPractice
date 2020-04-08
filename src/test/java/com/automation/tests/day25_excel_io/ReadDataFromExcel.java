77e640a7525092c9f88fd815913d6b897432dd29
Adil Doshayev <doshayev@gmail.com>
2020-04-07 19:54:43 -0500

    update

--- src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
+++ src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
@@ -2,13 +2,13 @@ package com.automation.tests.day25_excel_io;
 
 import org.apache.poi.ss.usermodel.*;
 import org.testng.annotations.Test;
-
 import java.io.File;
-
+/**
+ * PLEASE DON'T WORRY ABOUT QUESTION MARK ON THE EXCEL FILE ICON
+ */
 public class ReadDataFromExcel {
-
     @Test
-    public void readExcelFileTest() throws Exception{
+    public void readExcelFileTest() throws Exception {
         //we need to get a file as an object
         File file = new File("VytrackTestUsers.xlsx");
         //object that represents excel file
@@ -21,24 +21,28 @@ public class ReadDataFromExcel {
         Cell firstCell = firstRow.getCell(0);
         //get string value
         String value = firstCell.getStringCellValue();
-
         String secondCellValue = firstRow.getCell(1).getStringCellValue();
         System.out.println(value);
         System.out.println(secondCellValue);
-
         int lastCell = firstRow.getLastCellNum();
-        System.out.println("##########");
-        System.out.print("| ");
-
+        System.out.println("##################");
         for (int i = 0; i < lastCell; i++) {
-            System.out.print(firstRow.getCell(i)+" | ");
+            System.out.print(firstRow.getCell(i) + " | ");
         }
+        //last row is 16th --> index is 15
+        //index of last row
         int numberOfRows = workSheet.getLastRowNum();
+        //returns how many rows at all
         int numberOfRows2 = workSheet.getPhysicalNumberOfRows();
-
-        System.out.println("\nIndex of last row = " + numberOfRows);
-        System.out.println("\nNumber Of Rows = " + numberOfRows2);
-
-
+        System.out.println("\nIndex of last row   : " + numberOfRows);
+        System.out.println("\nNumber of rows      : " + numberOfRows2);
+        System.out.println("##########################");
+        for (int row = 0; row < workSheet.getPhysicalNumberOfRows(); row++) {
+            for (int cell = 0; cell < workSheet.getRow(row).getLastCellNum(); cell++) {
+                String cellValue = workSheet.getRow(row).getCell(cell).getStringCellValue();
+                System.out.print(cellValue + " | ");
+            }
+            System.out.println();
+        }
     }
 }
