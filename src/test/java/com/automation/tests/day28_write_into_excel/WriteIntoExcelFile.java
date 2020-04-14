d967fad79602232b234ac928f3a8b05f00be4feb
Adil Doshayev <doshayev@gmail.com>
2020-04-14 18:52:05 -0500

    update

--- src/test/java/com/automation/tests/day28_write_into_excel/WriteIntoExcelFile.java
+++ src/test/java/com/automation/tests/day28_write_into_excel/WriteIntoExcelFile.java
@@ -1,10 +1,21 @@
 package com.automation.tests.day28_write_into_excel;
 
+import org.apache.poi.ss.usermodel.*;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class WriteIntoExcelFile {
     @Test
-    public void writeIntoExcelFile(){
+    public void writeIntoExcelFile() throws IOException {
+        FileInputStream inputStream = new FileInputStream("VytrackTestUsers.xlsx");
+        Workbook workbook = WorkbookFactory.create(inputStream);
+        Sheet sheet = workbook.getSheet("QA3-short");
+        Row row = sheet.getRow(0);
+        Cell cell = row.getCell(row.getLastCellNum() - 1);
+        System.out.println(cell.getStringCellValue());
+
 
 
     }
